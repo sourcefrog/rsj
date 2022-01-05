4a1a0bd7ce9258754ad4ba210e4d1dc14c91fffe
Martin Pool <mbp@sourcefrog.net>
2022-01-05 06:57:28 -0800

    cargo-fuzz parsing

---
+++ fuzz/fuzz_targets/scan.rs
@@ -0,0 +1,8 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &str| {
+    // For any string, parsing it may or may not succeed, but it should not crash,
+    // hang, or panic.
+    let _ = rsj::parse::parse(data);
+});
