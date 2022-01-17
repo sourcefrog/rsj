768f5425e10dead72b161d07e4ecab3baf367953
Martin Pool <mbp@sourcefrog.net>
2022-01-17 11:09:22 -0800

    Fix up fuzz target for rename

--- fuzz/fuzz_targets/scan.rs
+++ fuzz/fuzz_targets/scan.rs
@@ -4,5 +4,5 @@ use libfuzzer_sys::fuzz_target;
 fuzz_target!(|data: &str| {
     // For any string, parsing it may or may not succeed, but it should not crash,
     // hang, or panic.
-    let _ = rsj::parse::parse(data);
+    let _ = rsj::scan::scan_sentence(data);
 });
